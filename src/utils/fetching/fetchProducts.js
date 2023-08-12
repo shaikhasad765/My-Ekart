19fc504b3e1b21683ce6dabfe5bc6434ac610cfd
shaikhasad765 <shaikhasad765@gmail.com>
2023-08-12 20:30:05 +0530

    Second commit

--- src/utils/fetching/fetchProducts.js
+++ src/utils/fetching/fetchProducts.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProducts = async () => {
   try {
     const prods = await axios(
-      "https://my-json-server.typicode.com/shaikhasad765/my-ecommerce-api/products"
+      "https://my-json-server.typicode.com/shaikhasad765/My-Ekart/products"
     );
 
     return prods;
