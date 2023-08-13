4d0073c197d9eec07db5c4de7c9733ac20fa3478
shaikhasad765 <shaikhasad765@gmail.com>
2023-08-13 17:12:22 +0530

    Commit

--- src/utils/fetching/fetchProducts.js
+++ src/utils/fetching/fetchProducts.js
@@ -1,19 +1,13 @@
-// Importing the Axios library
 import axios from "axios";
 
-// Async function to fetch products from the JSON server
 export const getProducts = async () => {
   try {
-    // Making a GET request to the JSON URL using Axios
-    const response = await axios.get(
+    const prods = await axios(
       "https://my-json-server.typicode.com/shaikhasad765/My-Ekart/products"
     );
 
-    // Returning the response containing the fetched products
-    return response.data;
+    return prods;
   } catch (err) {
-    // Handling errors if the fetching process fails
-    console.log("Error in fetching products:", err.message);
-    throw err; // Re-throwing the error to be handled at a higher level if needed
+    console.log("Error In fetching");
   }
 };
